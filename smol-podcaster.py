aa5b123e0588ed0a38c63b210f407be6c74886c2
chrisshaw <chrisstevenshaw@gmail.com>
2023-10-02 21:35:41 -0400

    Modify for user interviews

--- smol-podcaster.py
+++ smol-podcaster.py
@@ -14,17 +14,18 @@ load_dotenv()
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 anthropic = Anthropic(api_key=os.environ.get("ANTHROPIC_API_KEY"))
 
-def transcribe_audio(file_url, episode_name):
-    output = replicate.run(
-        "thomasmol/whisper-diarization:7e5dafea13d80265ea436e51a310ae5103b9f16e2039f54de4eede3060a61617",
-        input={
-            "file_url": file_url,
-            "num_speakers": 3,
-            "prompt": "A technical podcast about artificial intelligence and machine learning"
-        }
-    )
+def transcribe_audio(file_path, episode_name):
+    with open(file_path, "rb") as f:
+        output = replicate.run(
+            "thomasmol/whisper-diarization:7e5dafea13d80265ea436e51a310ae5103b9f16e2039f54de4eede3060a61617",
+            input={
+                "file": f,
+                "num_speakers": 2,
+                "prompt": "A user interview between an UPchieve edtech product manager and a student, volunteer tutor, or other user"
+            }
+        )
     
-    with open(f"./podcasts-raw-transcripts/{episode_name}.json", "w") as f:
+    with open(f"./interviews-raw-transcripts/{episode_name}.json", "w") as f:
         json.dump(output, f)
 
     return output['segments']
@@ -58,7 +59,7 @@ def process_transcript(transcript, episode_name):
         
     clean_transcript = "\n\n".join(transcript_strings)
     
-    with open(f"./podcasts-clean-transcripts/{episode_name}.md", "w") as f:    
+    with open(f"./interviews-clean-transcripts/{episode_name}.md", "w") as f:    
         f.write(clean_transcript)
         
     return clean_transcript
@@ -72,7 +73,7 @@ def create_chapters(transcript):
     chapters = anthropic.completions.create(
         model="claude-2",
         max_tokens_to_sample=3000,
-        prompt=f"{HUMAN_PROMPT} Here's a podcast transcript with timestamps. Generate a list of all major topics covered in the podcast, and the timestamp at which it's mentioned in the podcast. Use this format: - [00:00:00] Topic name. Here's the transcript: \n\n {transcript} {AI_PROMPT}",
+        prompt=f"{HUMAN_PROMPT} Here's an interview transcript with timestamps. Generate a list of all major topics covered in the podcast, and the timestamp at which it's mentioned in the podcast. Use this format: - [00:00:00] Topic name. Here's the transcript: \n\n {transcript} {AI_PROMPT}",
     )
     
     print(chapters.completion)
@@ -87,7 +88,7 @@ def create_show_notes(transcript):
     chapters = anthropic.completions.create(
         model="claude-2",
         max_tokens_to_sample=3000,
-        prompt=f"{HUMAN_PROMPT} I'll give you a podcast transcript; help me create a list of every company, person, project, or any other named entitiy that you find in it. Here's the transcript: \n\n {transcript} {AI_PROMPT}",
+        prompt=f"{HUMAN_PROMPT} I'll give you an interview transcript; help me create a list of every company, person, project, or any other named entitiy that you find in it. Here's the transcript: \n\n {transcript} {AI_PROMPT}",
     )
     
     print(chapters.completion)
@@ -102,7 +103,7 @@ def create_writeup(transcript):
     chapters = anthropic.completions.create(
         model="claude-2",
         max_tokens_to_sample=3000,
-        prompt=f"{HUMAN_PROMPT} You're the writing assistant of a podcast producer. For each episode, we do a write up to recap the core ideas of the episode and expand on them. Write a list of bullet points on topics we should expand on, and then 4-5 paragraphs about them. Here's the transcript: \n\n {transcript} {AI_PROMPT}",
+        prompt=f"{HUMAN_PROMPT} You're the writing assistant of a product manager. For each interview, we do a write up to recap the core ideas of the interview and further explore them. Write a list of key highlights we should further explore, such as core use cases, major customer or user problems, important user journeys or workflows, and key segmentation attributes. Here's the transcript: \n\n {transcript} {AI_PROMPT}",
     )
     
     print(chapters.completion)
@@ -113,13 +114,12 @@ def title_suggestions(transcript):
     prompt = f"""
     These are some titles of previous podcast episodes we've published:
 
-    1. "From RLHF to RLHB: The Case for Learning from Human Behavior"
-    2. "Commoditizing the Petaflop"
-    3. "Llama 2: The New Open LLM SOTA"
-    4. "FlashAttention 2: making Transformers 800\%\ faster w/o approximation"
-    5. "Mapping the future of *truly* Open Models and Training Dolly for $30"
+    1. "[Student] Interview with Justin Membreno and Chris Shaw (2023-09-25)"
+    2. "[Tutor] Interview with Kaya Mendez and Bailey Lowenthal (2023-10-01)"
+    3. "[School Counselor] Interview with Ken Norton and Grace Stevens (2023-08-11)"
 
-    Here's a transcript of the podcast episode; suggest 8 title options for it:
+
+    Here's a transcript of the interview; suggest 8 title options for it:
     
     {transcript}
     """
@@ -194,15 +194,13 @@ def tweet_suggestions(transcript):
     return suggestions
     
 def main():
-    parser = argparse.ArgumentParser(description="Transcribe the podcast audio from an URL like tmpfiles.")
-    parser.add_argument("url", help="The URL of the podcast to be processed.")
+    parser = argparse.ArgumentParser(description="Transcribe the podcast audio from a local file.")
+    parser.add_argument("file_path", help="The local file path of the podcast to be processed.")
     parser.add_argument("name", help="The name of the output transcript file without extension.")
-
     args = parser.parse_args()
 
-    url = args.url
+    file_path = args.file_path
     name = args.name
-    
     raw_transcript_path = f"./podcasts-raw-transcripts/{name}.json"
     clean_transcript_path = f"./podcasts-clean-transcripts/{name}.md"
     results_file_path = f"./podcasts-results/{name}.md"
